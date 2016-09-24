812a2cfdfeef75f340300d6e8631e0cecaa7bae8
Madeleine Schönemann <madeleine.schonemann@gmail.com>
2016-09-24 17:20:42 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+---
+layout: null
+---
+  $(document).ready(function() {
+    $('a.blog-button').click(function(e) {
+      if ($('.header-cover').hasClass('collapsed')) return
+      currentWidth = $('.header-cover').width()
+      if (currentWidth < 960) {
+        $('.header-cover').addClass('collapsed')
+        $('.content-wrapper').addClass('animated slideInRight')
+      } else {
+        $('.header-cover').css('max-width', currentWidth)
+        $('.header-cover').animate({
+          'max-width': '530px',
+          'width': '40%'
+        }, 400, swing = 'swing', function() {})
+      }
+    })
+
+    if (window.location.hash && window.location.hash == '#blog') {
+      $('.header-cover').addClass('collapsed')
+    }
+
+    if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
+      $('.header-cover').addClass('collapsed')
+    }
+
+    $('.mobile-menu').click(function() {
+      $('.navigation-wrapper').toggleClass('visible animated slideInDown')
+      $('.fa.fa-bars').toggleClass('hidden')
+      $('.fa.fa-times').toggleClass('visible hidden')
+    })
+
+    $('.navigation-wrapper .blog-button').click(function() {
+      $('.navigation-wrapper').toggleClass('visible')
+      $('.fa.fa-bars').toggleClass('hidden')
+      $('.fa.fa-times').toggleClass('visible hidden')
+    })
+
+    $(".archive h3").each(function() {
+      var archive = $(this);
+      var icon = archive.find("i");
+      var posts = archive.next("ul").slideUp();
+      var state = false;
+      $(archive).click(function() {
+        $(icon).toggleClass("fa-caret-right fa-caret-down");
+        state = !state;
+        posts.slideToggle(state);
+      });
+    });
+  })
